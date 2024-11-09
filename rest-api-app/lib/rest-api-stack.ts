35cf255e32a46a2e77511152b0e957a973ecf113
Mark Hogan <hoganmark2021@gmail.com>
2024-11-09 16:50:34 +0000

    you can now add games

--- rest-api-app/lib/rest-api-stack.ts
+++ rest-api-app/lib/rest-api-stack.ts
@@ -21,7 +21,7 @@ export class RestAPIStack extends cdk.Stack {
       tableName: "Games",
     });
 
-    
+
     // Functions 
     const getGameByIdFn = new lambdanode.NodejsFunction(
       this,
@@ -37,66 +37,81 @@ export class RestAPIStack extends cdk.Stack {
           REGION: 'us-east-1',
         },
       }
-      );
-      
-      const getAllGamesFn = new lambdanode.NodejsFunction(
-        this,
-        "GetAllGamesFn",
-        {
-          architecture: lambda.Architecture.ARM_64,
-          runtime: lambda.Runtime.NODEJS_18_X,
-          entry: `${__dirname}/../lambdas/getAllGames.ts`,
-          timeout: cdk.Duration.seconds(10),
-          memorySize: 128,
-          environment: {
-            TABLE_NAME: gamesTable.tableName,
-            REGION: 'us-east-1',
-          },
-        }
-        );
-        
-        new custom.AwsCustomResource(this, "gamesddbInitData", {
-          onCreate: {
-            service: "DynamoDB",
-            action: "batchWriteItem",
-            parameters: {
-              RequestItems: {
-                [gamesTable.tableName]: generateBatch(games),
-              },
-            },
-            physicalResourceId: custom.PhysicalResourceId.of("gamesddbInitData"),
-          },
-          policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
-            resources: [gamesTable.tableArn],
-          }),
-        });
-        gamesTable.grantReadData(getGameByIdFn)
-        gamesTable.grantReadData(getAllGamesFn)
+    );
 
-        const api = new apig.RestApi(this, "RestAPI", {
-          description: "demo api",
-          deployOptions: {
-            stageName: "dev",
-          },
-          defaultCorsPreflightOptions: {
-            allowHeaders: ["Content-Type", "X-Amz-Date"],
-            allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
-            allowCredentials: true,
-            allowOrigins: ["*"],
-          },
-        });
-
-        const gamesEndpoint = api.root.addResource("games");
-        gamesEndpoint.addMethod(
-          "GET",
-          new apig.LambdaIntegration(getAllGamesFn, { proxy: true })
-        );
-    
-        const gameEndpoint = gamesEndpoint.addResource("{gameId}");
-        gameEndpoint.addMethod(
-          "GET",
-          new apig.LambdaIntegration(getGameByIdFn, { proxy: true })
-        );
+    const getAllGamesFn = new lambdanode.NodejsFunction(
+      this,
+      "GetAllGamesFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/getAllGames.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: gamesTable.tableName,
+          REGION: 'us-east-1',
+        },
       }
-    }
+    );
+    const newGameFn = new lambdanode.NodejsFunction(this, "AddGameFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/addGame.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: gamesTable.tableName,
+        REGION: "us-east-1",
+      },
+    });
+
+    new custom.AwsCustomResource(this, "gamesddbInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [gamesTable.tableName]: generateBatch(games),
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("gamesddbInitData"),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [gamesTable.tableArn],
+      }),
+    });
+    gamesTable.grantReadData(getGameByIdFn)
+    gamesTable.grantReadData(getAllGamesFn)
+    gamesTable.grantReadWriteData(newGameFn)
+
+    const api = new apig.RestApi(this, "RestAPI", {
+      description: "demo api",
+      deployOptions: {
+        stageName: "dev",
+      },
+      defaultCorsPreflightOptions: {
+        allowHeaders: ["Content-Type", "X-Amz-Date"],
+        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowCredentials: true,
+        allowOrigins: ["*"],
+      },
+    });
+
+    const gamesEndpoint = api.root.addResource("games");
+    gamesEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getAllGamesFn, { proxy: true })
+    );
     
+    gamesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newGameFn, { proxy: true })
+    );
+    const gameEndpoint = gamesEndpoint.addResource("{gameId}");
+    gameEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getGameByIdFn, { proxy: true })
+    );
+  }
+}
